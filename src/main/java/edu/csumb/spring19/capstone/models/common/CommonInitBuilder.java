76000ebb38cad7e916aec660723c151125bb1683
ChristopherLeeWilliams <chris_lw16@yahoo.com>
2020-04-09 20:50:23 -0700

    Added framework for shippers and use of shipperIDs, currently disabled.

--- src/main/java/edu/csumb/spring19/capstone/models/common/CommonInitBuilder.java
+++ src/main/java/edu/csumb/spring19/capstone/models/common/CommonInitBuilder.java
@@ -10,89 +10,58 @@ public class CommonInitBuilder {
     public List<CommonData> build() {
         return common;
     }
-
-    public CommonInitBuilder ranches() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "Baillie");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "Spreckles");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("ranches", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder fertilizers() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "0-0-20");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "6-16-0 (LIQ)");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("fertilizers", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder chemicals() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "Kerb");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "RoundUp");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("chemicals", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder chemicalRateUnits() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "Gallons");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "Pounds");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("chemicalRateUnits", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder irrigationMethod() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "Drip");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "Furrow");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("irrigationMethod", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder irrigators() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "John Smith");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "Jane Doe");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("irrigators", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder tractorOperators() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "Dave Smith");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "Erica Doe");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("tractorOperators", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
-    public CommonInitBuilder tractorWork() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "Initial Setup");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "Cultivate");
-        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("tractorWork", new ArrayList<>());
-        common.add(list);
-        return this;
-    }
-
     public CommonInitBuilder bedTypes() {
-        // CommonDataValue<String> cdv1 = new CommonDataValue<String>((new ObjectId()).toString(), "20-20");
-        // CommonDataValue<String> cdv2 = new CommonDataValue<String>((new ObjectId()).toString(), "40");
         CommonData<List<CommonDataValue<String>>> list = new CommonData<>("bedTypes", new ArrayList<>());
         common.add(list);
         return this;
     }
-
+    public CommonInitBuilder chemicalRateUnits() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("chemicalRateUnits", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder chemicals() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("chemicals", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
     public CommonInitBuilder commodities() {
-        // HashMap<String, List<String>> innerHash = new HashMap<String, List<String>>();
-        // innerHash.put("Lettuce", Arrays.asList("Variety 1", "Variety 2"));
-        // CommonDataValue<HashMap<String, List<String>>> cdv1 = new CommonDataValue<HashMap<String, List<String>>>((new ObjectId()).toString(), innerHash);
-        
-        // HashMap<String, List<String>> innerHash2 = new HashMap<String, List<String>>();
-        // innerHash2.put("Cabbage", Arrays.asList("Variety 1", "Variety 2"));
-        // CommonDataValue<HashMap<String, List<String>>> cdv2 = new CommonDataValue<HashMap<String, List<String>>>((new ObjectId()).toString(), innerHash2);
-        
         CommonData<List<CommonDataValue<HashMap<String, List<String>>>>> list = new CommonData<>("commodities", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder fertilizers() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("fertilizers", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder irrigationMethod() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("irrigationMethod", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder irrigators() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("irrigators", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder ranches() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("ranches", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder shippers() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("shippers", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder tractorOperators() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("tractorOperators", new ArrayList<>());
+        common.add(list);
+        return this;
+    }
+    public CommonInitBuilder tractorWork() {
+        CommonData<List<CommonDataValue<String>>> list = new CommonData<>("tractorWork", new ArrayList<>());
         common.add(list);
         return this;
     }
--- src/main/java/edu/csumb/spring19/capstone/models/common/CommonInitService.java
+++ src/main/java/edu/csumb/spring19/capstone/models/common/CommonInitService.java
@@ -10,48 +10,41 @@ public class CommonInitService {
     private CommonRepository commonRepository;
 
     public void initDatabase() {
-        if (!commonRepository.existsById("ranches")) {
-            commonRepository.saveAll((new CommonInitBuilder()).ranches().build());
-        }
-
-        if (!commonRepository.existsById("fertilizers")) {
-            commonRepository.saveAll((new CommonInitBuilder()).fertilizers().build());
-        }
-
-        if (!commonRepository.existsById("chemicals")) {
-            commonRepository.saveAll((new CommonInitBuilder()).chemicals().build());
-        }
-
-        if (!commonRepository.existsById("chemicalRateUnits")) {
-            commonRepository.saveAll((new CommonInitBuilder()).chemicalRateUnits().build());
-        }
-
-        if (!commonRepository.existsById("irrigationMethod")) {
-            commonRepository.saveAll((new CommonInitBuilder()).irrigationMethod().build());
-        }
-
-        if (!commonRepository.existsById("irrigators")) {
-            commonRepository.saveAll((new CommonInitBuilder()).irrigators().build());
-        }
-
-        if (!commonRepository.existsById("tractorOperators")) {
-            commonRepository.saveAll((new CommonInitBuilder()).tractorOperators().build());
-        }
-
-        if (!commonRepository.existsById("tractorWork")) {
-            commonRepository.saveAll((new CommonInitBuilder()).tractorWork().build());
-        }
-
-        if (!commonRepository.existsById("bedTypes")) {
-            commonRepository.saveAll((new CommonInitBuilder()).bedTypes().build());
-        }
-
         if (commonRepository.existsById("bedCounts")) {
             commonRepository.deleteById("bedCounts");
         }
-
+        if (!commonRepository.existsById("bedTypes")) {
+            commonRepository.saveAll((new CommonInitBuilder()).bedTypes().build());
+        }
+        if (!commonRepository.existsById("chemicalRateUnits")) {
+            commonRepository.saveAll((new CommonInitBuilder()).chemicalRateUnits().build());
+        }
+        if (!commonRepository.existsById("chemicals")) {
+            commonRepository.saveAll((new CommonInitBuilder()).chemicals().build());
+        }
         if (!commonRepository.existsById("commodities")) {
             commonRepository.saveAll((new CommonInitBuilder()).commodities().build());
+        }
+        if (!commonRepository.existsById("fertilizers")) {
+            commonRepository.saveAll((new CommonInitBuilder()).fertilizers().build());
+        }
+        if (!commonRepository.existsById("irrigationMethod")) {
+            commonRepository.saveAll((new CommonInitBuilder()).irrigationMethod().build());
+        }
+        if (!commonRepository.existsById("irrigators")) {
+            commonRepository.saveAll((new CommonInitBuilder()).irrigators().build());
+        }
+        if (!commonRepository.existsById("ranches")) {
+            commonRepository.saveAll((new CommonInitBuilder()).ranches().build());
+        }
+        if (!commonRepository.existsById("shippers")) {
+            commonRepository.saveAll((new CommonInitBuilder()).shippers().build());
+        }
+        if (!commonRepository.existsById("tractorOperators")) {
+            commonRepository.saveAll((new CommonInitBuilder()).tractorOperators().build());
+        }
+        if (!commonRepository.existsById("tractorWork")) {
+            commonRepository.saveAll((new CommonInitBuilder()).tractorWork().build());
         }
     }
 }
